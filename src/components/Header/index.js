26f0e9712489dd67d3c6d3a07a378e92affda38d
Thomas-279 <67013005+Thomas-279@users.noreply.github.com>
2021-02-22 17:42:32 +0100

    base

---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+import './style.scss';
+
+// == Composant
+const Header = () => (
+  <div className="header">
+    <div className="header__top">
+      <h1>Thomas Garraut</h1>
+    </div>
+  </div>
+);
+
+// == Export
+export default Header;
