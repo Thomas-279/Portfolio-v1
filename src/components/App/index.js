147785c7e74dc8d847ce73fe647971bbfa8b986b
Thomas-279 <67013005+Thomas-279@users.noreply.github.com>
2021-02-24 17:25:51 +0100

    test

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,12 +3,14 @@ import React from 'react';
 
 // == Import
 import Header from 'src/components/Header';
+import Home from 'src/components/Home';
 import './style.scss';
 
 // == Composant
 const App = () => (
   <div className="app">
     <Header />
+    <Home />
   </div>
 );
 
---
+++ src/components/Home/index.js
@@ -0,0 +1,17 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+import './style.scss';
+
+// == Composant
+const Home = () => (
+  <div className="home">
+    <div className="home__top">
+      <h1>Je suis dans home</h1>
+    </div>
+  </div>
+);
+
+// == Export
+export default Home;
