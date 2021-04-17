5a4ad7996e8ae30a528f508550c6d5b591e354c0
Thomas-279 <67013005+Thomas-279@users.noreply.github.com>
2021-04-17 16:27:28 +0200

    footer + button

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,21 +4,31 @@ import { Link } from 'react-router-dom';
 
 // == Import
 import './style.scss';
+import linkedin from 'src/assets/images/img-logo/linkedin.svg';
+import github from 'src/assets/images/img-logo/github.svg';
+import email from 'src/assets/images/img-logo/email.svg';
 
 // == Composant
 const Footer = () => (
-  <div className="footer">
-    <ul className="footer__logo">
-      <li className="footer__logo-list">
-        <p>linkedin</p>
-      </li>
-      <li className="footer__logo-list">
-        <p>linkedin</p>
-      </li>
-      <li className="footer__logo-list">
-        <p>linkedin</p>
-      </li>
-    </ul>
+  <div className="footer__container">
+    <h3 className="footer__title">Contact</h3>
+    <div className="link__container">
+      <div className="link__content">
+        <Link to={{ pathname: 'https://www.linkedin.com/in/thomas-garraut/' }} target="_blank">
+          <img src={linkedin} alt="linkedin" />
+        </Link>
+      </div>
+      <div className="link__content">
+        <a href="mailto:thomas.garraut@gmail.com" target="_blank" rel="noopener noreferrer">
+          <img src={email} alt="email" className="link__images" />
+        </a>
+      </div>
+      <div className="link__content">
+        <Link to={{ pathname: 'https://github.com/Thomas-279' }} target="_blank">
+          <img src={github} alt="github" />
+        </Link>
+      </div>
+    </div>
   </div>
 );
 
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,9 +5,6 @@ import { Link } from 'react-router-dom';
 // == Import
 import './style.scss';
 import profilePicture from 'src/assets/cv/small.jpg';
-import linkedin from 'src/assets/images/img-logo/linkedin.svg';
-import github from 'src/assets/images/img-logo/github.svg';
-import email from 'src/assets/images/img-logo/email.svg';
 
 // == Composant
 const Home = () => (
@@ -20,43 +17,30 @@ const Home = () => (
           <h1 className="home__part-title">Thomas GARRAUT</h1>
           <h2 className="home__part-subtitle">Développeur web fullstack Javascript</h2>
           <p className="home__part-content">Node.js / React - Redux</p>
+
         </div>
       </section>
       {/* Partie 2 contactez moi */}
       <section className="home__part2">
         <div className="home__part2-content">
-          <p>Après une carrière en restauration, dont 5 en tant que directeur de restaurant,
+          <p>Après une carrière en restauration, dont 5 années en tant que directeur de restaurant,
             j'ai pu acquérir de solides compétences en gestion d'entreprise,
             en organisation et en management d'équipe.
           </p>
-          <p>Pour nouveau challenge, j'ai fais le choix d'un virage à 180° dans le développement
+          <p>Pour nouveau challenge, j'ai fait le choix d'un virage à 180° dans le développement
             web et mobile au cours de l'année 2020.
           </p>
+          <Link
+            to={{ pathname: 'https://www.linkedin.com/in/thomas-garraut/detail/overlay-view/urn:li:fsd_profileTreasuryMedia:(ACoAABS9U2YBgGJQx8VCfPht1Mynjo38caJyzdU,1635452955800)' }}
+            target="_blank"
+          >
+            <button type="button" className="button type1">
+              Consulter mon CV sur Linkedin
+            </button>
+          </Link>
         </div>
         <div className="home__part2-picture">
           <img src={profilePicture} alt="ThomasGarraut" className="profile-picture" />
-        </div>
-      </section>
-      <section className="home__part3">
-        <div className="contact__container">
-          <h3 className="contact__title">Contact</h3>
-          <div className="link__container">
-            <div className="link__content">
-              <Link to={{ pathname: 'https://www.linkedin.com/in/thomas-garraut/' }} target="_blank">
-                <img src={linkedin} alt="linkedin" />
-              </Link>
-            </div>
-            <div className="link__content">
-              <a href="mailto:thomas.garraut@gmail.com" target="_blank" rel="noopener noreferrer">
-                <img src={email} alt="email" className="link__images" />
-              </a>
-            </div>
-            <div className="link__content">
-              <Link to={{ pathname: 'https://github.com/Thomas-279' }} target="_blank">
-                <img src={github} alt="github" />
-              </Link>
-            </div>
-          </div>
         </div>
       </section>
     </div>
