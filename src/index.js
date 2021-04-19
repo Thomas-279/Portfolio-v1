741fd724465b1ca1bc132fb9aaf904528dae8ec3
Thomas-279 <67013005+Thomas-279@users.noreply.github.com>
2021-04-19 17:37:12 +0200

    npm deploy

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import {
   BrowserRouter as Router,
+  Route,
 } from 'react-router-dom';
 
 // == Import : local
@@ -13,8 +14,8 @@ import App from 'src/components/App';
 // 1. Élément React racine (celui qui contient l'ensemble de l'app)
 //    => crée une structure d'objets imbriqués (DOM virtuel)
 const rootReactElement = (
-  <Router>
-    <App />
+  <Router basename={process.env.PUBLIC_URL}>
+    <Route exact path="/" component={App} />
   </Router>
 );
 // 2. La cible du DOM (là où la structure doit prendre vie dans le DOM)
