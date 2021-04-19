b31a4676d36b65291c331f5765ac11cf610ec9d2
Thomas-279 <67013005+Thomas-279@users.noreply.github.com>
2021-04-19 18:20:17 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import App from 'src/components/App';
 // 1. Élément React racine (celui qui contient l'ensemble de l'app)
 //    => crée une structure d'objets imbriqués (DOM virtuel)
 const rootReactElement = (
-  <Router basename="/">
+  <Router>
     <App />
   </Router>
 );
