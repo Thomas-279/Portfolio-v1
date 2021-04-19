34139e68be12394113dbba96ea84b8cf4cd38b9e
Thomas-279 <67013005+Thomas-279@users.noreply.github.com>
2021-04-19 18:28:06 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import {
-  HashRouter as Router,
+  BrowserRouter as Router,
 } from 'react-router-dom';
 
 // == Import : local
@@ -13,7 +13,7 @@ import App from 'src/components/App';
 // 1. Élément React racine (celui qui contient l'ensemble de l'app)
 //    => crée une structure d'objets imbriqués (DOM virtuel)
 const rootReactElement = (
-  <Router basename={process.env.PUBLIC_URL}>
+  <Router>
     <App />
   </Router>
 );
